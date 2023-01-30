1bf467ba5a4ee059d080aa987146f72bf8a7c888
Voncloft <voncloft@gmail.com>
2023-01-30 10:21:56 -0500

    Minor Updates

--- functions/get_summary.php
+++ functions/get_summary.php
@@ -10,9 +10,13 @@
 		echo "<table border='2'><caption>This Month</caption><th>Date</th><th>Description</th><th>Amount</th>";
 		foreach($rows as $financial_transaction)
 		{
-			echo "<tr><td contenteditable>".$financial_transaction['day_of_month']."</td><td contenteditable>".$financial_transaction['description']."</td><td contenteditable>".$financial_transaction['amount']."</td></tr>";
+			//echo '<tr><td contenteditable>'.$financial_transaction["day_of_month"].'</td><td contenteditable onBlur="saveToDb(this,".$financial_transaction["id"].",description")">'.$financial_transaction["description"]'"</td><td contenteditable onBlur=saveToDb(this,'.$financial_transaction["id"].',\"amount\")">'.$financial_transaction["amount"].'</td></tr>';
+			echo '<tr><td contenteditable onBlur="saveToDb(this,\'day_of_month\')">'.$financial_transaction['day_of_month'].'</td>';
+			echo '<td contenteditable onBlur="saveToDb(this,\'description\')">'.$financial_transaction['description'].'</td>';
+			echo '<td contenteditable onBlur="saveToDb(this,\'amount\')">'.$financial_transaction['amount'].'</td></tr>';
 		}
 		echo "</table>";
 		echo "</td></tr><tr><td align='center'><a href='../index.php'>Home</a></td></tr></table>";
 	}
+	include '../ajax/db_functions.js';
 ?>
